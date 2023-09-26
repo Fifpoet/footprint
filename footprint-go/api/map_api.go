0a99227f30e3fdf28a1c250a16c43657180fd6eb
Nameless_Student <2841221245@qq.com>
2023-09-26 15:46:44 +0800

    hotfix_cyz_补充map实体和功能框架

---
+++ footprint-go/api/map_api.go
@@ -0,0 +1,17 @@
+// package api
+
+// import(
+// 	"github.com/fifpoet/footprint/dao"
+// 	"github.com/fifpoet/footprint/global"
+// 	"github.com/fifpoet/footprint/model"
+// 	"github.com/fifpoet/footprint/service"
+// 	"github.com/gin-gonic/gin"
+// 	"net/http"
+// )
+
+// // 进入map界面的初始化
+// func Map(c *gin.Context){
+// 	MapInit := &model.Map{}
+// 	// 读取 db 地图信息
+// 	dbMapInit, err :=
+// }
